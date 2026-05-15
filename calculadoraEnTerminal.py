3b2e36cb94c6cea3bed34e31a2d422d9264e278d
Angel <arodriguez121@ucol.mx>
2026-05-14 19:37:01 -0600

    Ahora imprime los resultados

--- calculadoraEnTerminal.py
+++ calculadoraEnTerminal.py
@@ -1,20 +1,20 @@
 def sumar(x, y):
     return x+y
 
-sumar(5,5)
+print(sumar(5,5))
 
 def restar(x, y):
     return x-y
 
-restar(9,3)
+print(restar(9,3))
 
 def multiplicar(x, y):
     return x*y
 
-multiplicar(5,5)
+print(multiplicar(5,5))
 
 def dividir(x, y):
     return x/y
 
-dividir(20,5)
+print(dividir(20,5))
   
