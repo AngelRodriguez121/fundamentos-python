6c2f59cf4ac39d4bc0dfbfaee99d1daa0e15466a
Angel <arodriguez121@ucol.mx>
2026-05-17 11:40:14 -0600

    If del menú anidados

--- calculadoraEnTerminal.py
+++ calculadoraEnTerminal.py
@@ -18,5 +18,23 @@ def dividir(x, y):
 
 print(dividir(20,5))
   
-menu = input("Menu:\n 1.-Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir\nIngresa la opción deseada")
+menu = int(input("Menu:\n 1.-Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir\nIngresa la opción deseada"))
 
+if menu == 1:
+    x = int(input("Ingresa el primer valor: "))
+    y = int(input("Ingresa el segundo valor: "))
+    print(sumar(x,y))
+elif menu == 2:
+    x = int(input("Ingresa el primer valor: "))
+    y = int(input("Ingresa el segundo valor: "))
+    print(restar(x,y))
+elif menu == 3:
+    x = int(input("Ingresa el primer valor: "))
+    y = int(input("Ingresa el segundo valor: "))
+    print(multiplicar(x,y))
+elif menu == 4:
+    x = int(input("Ingresa el primer valor: "))
+    y = int(input("Ingresa el segundo valor: "))
+    print(dividir(x,y))
+else:
+    print("Opcion no valida, intente de nuevo.")
